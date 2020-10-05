b77c693e1e76b9316dbafb09495fb36d3801ea11
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-05 15:32:51 +0100

    Add top_up method that adds money to card balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,10 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
     end
   end
+  describe '#top_up' do
+    it 'adds to the balance of the card' do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+  end
 end
