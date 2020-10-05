01f9a10ff8028f583f33a8ce914ce81043c0b61b
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-05 15:08:08 +0100

    First test checking for initial balance of 0 is now passing.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  describe '#initialize' do
+    it 'should have balance of 0 on initialization' do
+      expect(subject.balance).to eq(0)
+    end
+  end
 end
